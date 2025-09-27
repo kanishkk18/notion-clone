404ec0423932dfbd21791d9874ede562a998a013
kanishkk Bansal <168306760+kanishkk18@users.noreply.github.com>
2025-09-28 01:04:52 +0530

    Complete Migration to Pages Router with Prisma and NextAuth

--- app/(public)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-const PublicLayout = ({ children }: PropsWithChildren) => {
-  return <div className="h-full dark:bg-[#1F1F1F]">{children}</div>;
-};
-
-export default PublicLayout;
